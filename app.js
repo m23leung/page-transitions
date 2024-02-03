2996189d0f2758c0c24bc96b4476207e90e299dc
Mark <crimsondarkn@192-168-1-107.tpgi.com.au>
2024-02-03 21:08:56 +1100

    fix up animations

--- app.js
+++ app.js
@@ -21,17 +21,18 @@ const leaveAnimation = (current, done) => {
     );
 };
 
-const enterAnimation = (current, done) => {
+const enterAnimation = (current, done, gradient) => {
     const product = current.querySelector('.image-container');
     const text = current.querySelector('.showcase-text');
     const circles = current.querySelectorAll('.circle');
     const arrow = current.querySelector('.showcase-arrow');
 
     return (
-        tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
-        tlLeave.fromTo(product, { opacity: 0, y: -100 }, { opacity: 0, y: 1 }, "<"),
-        tlLeave.fromTo(text, { opacity: 0, y: 100 }, { opacity: 1, y: 0, onComplete: done }, "<"),
-        tlLeave.fromTo(circles, { opacity: 0, y: -200 }, { opacity: 1, y: 0, stagger: 0.15, ease: 'back.out(1.7)', duration: 1, }, "<")
+        tlEnter.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+        tlEnter.to('body', { background: gradient }, "<"),
+        tlEnter.fromTo(product, { opacity: 0, y: -100 }, { opacity: 1, y: 1 }, "<"),
+        tlEnter.fromTo(text, { opacity: 0, y: 100 }, { opacity: 1, y: 0, onComplete: done }, "<"),
+        tlEnter.fromTo(circles, { opacity: 0, y: -200 }, { opacity: 1, y: 0, stagger: 0.15, ease: 'back.out(1.7)', duration: 1, }, "<")
     );
 };
 
@@ -42,6 +43,19 @@ barba.init({
         // showcase transitions
         {
             name: 'default',
+
+            // Run once on page refresh
+            once(data) {
+                const done = this.async();
+                let next = data.next.container;
+                let gradient = getGradient(data.next.namespace);
+
+                // On page load, set the body background color
+                gsap.set('body', { background: gradient });
+
+                // Start enter animation
+                enterAnimation(next, done, gradient);
+            },
             leave(data) {
                 const done = this.async();
                 let current = data.current.container;
@@ -50,8 +64,21 @@ barba.init({
             enter(data) {
                 const done = this.async();
                 let next = data.next.container;
-                enterAnimation(next, done);
+                let gradient = getGradient(data.next.namespace);
+                enterAnimation(next, done, gradient);
             }
         }
     ]
 })
+
+// changing gradient on showcase
+getGradient = (name) => {
+    switch(name) {
+        case "handbag":
+            return "linear-gradient(260deg, #b75d62, #754d4f)";
+        case "boot":
+            return "linear-gradient(260deg, #5d8cb7, #4c4f70)";
+        case "hat":
+            return "linear-gradient(260deg, #b27a5c, #7f5450)";  
+    }
+}
